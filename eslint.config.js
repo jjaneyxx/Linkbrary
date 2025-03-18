077c9b97edb755868f67a77e3e2a1021bbf5bffe
jjaneyxx <hmyeonge@gmail.com>
2025-03-19 06:01:50 +0900

    chore: ESLint 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import configPrettier from "eslint-config-prettier";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+
+  pluginJs.configs.recommended, // JS 권장 규칙
+  ...tseslint.configs.recommended, // TS 권장 규칙
+
+  // React 권장 규칙
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    ...pluginReact.configs.flat.recommended,
+    // React 17 + : 버전을 자동 감지 후 불필요한 경고 방지
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+    // React 17 + : import 문 없이도 JSX 사용 가능
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "react/react-in-jsx-scope": "off",
     },
   },
-)
+
+  // Prettier 과 ESLint 충돌 시 Prettier 우선 적용
+  configPrettier,
+];
