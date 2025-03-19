c378e3102e3a24a69a5329e4166eaca72709e946
jjaneyxx <hmyeonge@gmail.com>
2025-03-19 13:53:37 +0900

    chore: Prettier, ESLint 설정 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import configPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import prettierConfig from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
 
-  pluginJs.configs.recommended, // JS 권장 규칙
-  ...tseslint.configs.recommended, // TS 권장 규칙
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 
   // React 권장 규칙
   {
@@ -18,15 +18,15 @@ export default [
     // React 17 + : 버전을 자동 감지 후 불필요한 경고 방지
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     // React 17 + : import 문 없이도 JSX 사용 가능
     rules: {
-      "react/react-in-jsx-scope": "off",
+      'react/react-in-jsx-scope': 'off',
     },
   },
 
   // Prettier 과 ESLint 충돌 시 Prettier 우선 적용
-  configPrettier,
+  prettierConfig,
 ];
