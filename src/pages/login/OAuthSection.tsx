e47b7a53dff293cf47b9a7214164a94f6d7b34df
jjaneyxx <hmyeonge@gmail.com>
2025-03-23 15:51:54 +0900

    design: 소셜 로그인 OauthSecion 마크업

--- src/pages/login/OAuthSection.tsx
+++ src/pages/login/OAuthSection.tsx
@@ -3,11 +3,15 @@ import kakaoLogin from '../../assets/images/kakao-login.svg';
 
 const OAuthSection = () => {
   return (
-    <div className="bg-gray-200 border-gray-300 px-4 py-3 ">
-      <span>소셜로그인</span>
-      <div className="flex">
-        <img src={googleLogin} />
-        <img src={kakaoLogin} />
+    <div className="bg-gray-200 border border-gray-300 px-6 py-3 w-full flex justify-between rounded-lg items-center">
+      <span className="text-[#373740]">소셜 로그인</span>
+      <div className="flex gap-4 ">
+        <button className="cursor-pointer">
+          <img src={googleLogin} alt="구글 로그인" />
+        </button>
+        <button className="cursor-pointer">
+          <img src={kakaoLogin} alt="카카오 로그인" />
+        </button>
       </div>
     </div>
   );
