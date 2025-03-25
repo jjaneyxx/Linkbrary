8fac7067041bd02c8552eea2ea966995f17fc803
jjaneyxx <hmyeonge@gmail.com>
2025-03-25 12:24:44 +0900

    refactor: 코드리뷰에 따른 /login 수정

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@ import OAuthSection from './OAuthSection';
 import Button from './Button';
 import AuthPrompt from '../../components/common/auth/AuthPrompt';
 import InputWithError from '../../components/common/auth/InputWithError';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../api/axios';
 import axios from 'axios';
@@ -10,8 +10,6 @@ import axios from 'axios';
 const Login: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
-  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true);
   const navigate = useNavigate();
 
   const handleLoginSuccess = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -21,7 +19,7 @@ const Login: React.FC = () => {
     if (!isEmailValid || !isPasswordValid) {
       return;
     }
-
+    // API 로직
     const formData = {
       email,
       password,
@@ -40,25 +38,31 @@ const Login: React.FC = () => {
     }
   };
 
-  const handleEmailInputValid = (e: React.FocusEvent<HTMLInputElement>) => {
-    const email = e.target.value;
+  const isEmailValid = useMemo(() => {
     const emailRegEx =
       /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
-    setIsEmailValid(emailRegEx.test(email));
-  };
+    if (email === '') {
+      return true;
+    } else {
+      return emailRegEx.test(email);
+    }
+  }, [email]);
 
-  const handlePasswordInputValid = (e: React.FocusEvent<HTMLInputElement>) => {
-    const password = e.target.value;
-    setIsPasswordValid(password.length >= 8);
-  };
+  const isPasswordValid = useMemo(() => {
+    if (password === '') {
+      return true;
+    } else {
+      return password.length >= 8;
+    }
+  }, [password]);
 
   return (
     <div className="flex flex-col items-center">
       <AuthPrompt prompt="회원이 아니신가요?" linkText="회원 가입하기" linkTo="/signup" />
 
       <form onSubmit={handleLoginSuccess}>
+        <InputWithError.Label label="이메일" id="user-email" />
         <InputWithError
-          label="이메일"
           id="user-email"
           type="email"
           name="email"
@@ -66,24 +70,21 @@ const Login: React.FC = () => {
           placeholder="이메일을 입력하세요"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          marginBottom="mb-4"
-          onBlur={handleEmailInputValid}
-          isInvalid={!isEmailValid}
+          className="mb-4"
+          disabled={!isEmailValid}
           errorMessage="이메일 형식으로 작성해 주세요."
         />
 
+        <InputWithError.Label label="비밀번호" id="password" />
         <InputWithError
-          label="비밀번호"
-          id="password"
           type="password"
           name="password"
-          autoComplete="password"
+          autoComplete="new-password"
           placeholder="비밀번호를 입력하세요"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          marginBottom="mb-[30px]"
-          onBlur={handlePasswordInputValid}
-          isInvalid={!isPasswordValid}
+          className="mb-4"
+          disabled={!isPasswordValid}
           errorMessage="비밀번호는 8자 이상 작성해 주세요."
         />
         <Button text="로그인" className="w-[400px] h-[53px] mb-8 " />
