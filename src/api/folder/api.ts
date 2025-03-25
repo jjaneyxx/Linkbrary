b1a0ef1bcce23e38ee16e869129aaa5575dba78f
jjaneyxx <hmyeonge@gmail.com>
2025-03-25 16:23:33 +0900

    feat: add folder api

---
+++ src/api/folder/api.ts
@@ -0,0 +1,64 @@
+import { apiClient } from '../apiClient';
+
+interface GetFoldersResponse {
+  id: number;
+  createdAt: string;
+  name: string;
+  linkCount: number;
+}
+
+interface PostFolderBody {
+  name: string;
+}
+
+interface PostFoldersResponse {
+  id: number;
+  createdAt: string;
+  name: string;
+}
+
+interface GetFolderByIdResponse {
+  id: number;
+  createdAt: string;
+  name: string;
+}
+
+// folder delete (4xx)
+interface DeleteFolderByIdResponse {
+  message: string;
+}
+
+interface PutFolderByIdResponse {
+  id: number;
+  createdAt: string;
+  name: string;
+}
+
+// get All Folders
+export const getAllFolders = async () => {
+  const response = await apiClient.get<GetFoldersResponse[]>('/folders');
+  return response.data;
+};
+
+// post
+export const postFolder = async (data: PostFolderBody) => {
+  const response = await apiClient.post<PostFoldersResponse>('/folders', data);
+  return response.data;
+};
+
+// get
+export const getFolderById = async ({ folderId }: { folderId: number }) => {
+  const response = await apiClient.get<GetFolderByIdResponse>(`/folders/${folderId}`);
+  return response.data;
+};
+
+// delete
+export const deleteFolderById = async ({ folderId }: { folderId: number }) => {
+  return await apiClient.delete<DeleteFolderByIdResponse>(`/folders/${folderId}`);
+};
+
+// put
+export const putFolderById = async ({ folderId, name }: { folderId: number; name: string }) => {
+  const response = await apiClient.put<PutFolderByIdResponse>(`/folders/${folderId}`, { name });
+  return response.data;
+};
