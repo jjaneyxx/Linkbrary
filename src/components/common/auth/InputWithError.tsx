5fc7da7f42a14a6dba06d63d5e9e42b442a1955e
해명 <hmyeonge@gmail.com>
2025-03-25 12:31:17 +0900

    Refactor/auth : 코드리뷰 반영 (#7)
    * refactor: 코드 리뷰 반영
    
    - 불필요한 상태 변수 (isEmailValid, isUserNameValid, ... ) 제거
    - onBlur > onChange 로 에러 표시 이벤트 핸들러 변경
    - InputError 컴포넌트 불필요한 props 제거(input 태그 기본 속성)
    - Label 을 별도 컴포넌트로 분리
    - 변수 isInValid > disabled (html 속성) 으로 대체
    
    * fix: 첫 화면에서 이메일, 비밀번호 에러 메시지가 렌더링되는 문제 해결
    
    * refactor: 코드리뷰에 따른 /login 수정

--- src/components/common/auth/InputWithError.tsx
+++ src/components/common/auth/InputWithError.tsx
@@ -2,53 +2,30 @@ import ErrorMessage from '../../../pages/login/ErrorMessage';
 import clsx from 'clsx';
 
 type InputProps = {
-  label: string;
-  id: string;
-  type: string; // email, password
-  name: string;
-  value: string;
-  autoComplete?: string;
-  placeholder: string;
-  marginBottom?: string;
-  isInvalid: boolean;
+  className?: string;
   errorMessage: string;
-  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+// Label 컴포넌트 분리
+export const Label = ({ label, id }: { label: string; id: string }) => {
+  return <label htmlFor={id}>{label}</label>;
 };
 
-const InputWithError = ({
-  label,
-  id,
-  type,
-  name,
-  value,
-  autoComplete,
-  placeholder,
-  marginBottom,
-  isInvalid,
-  onBlur,
-  onChange,
-  errorMessage,
-}: InputProps) => {
+const InputWithError = ({ className, disabled, errorMessage, ...rest }: InputProps) => {
   return (
-    <div className={`flex flex-col gap-3 mb-6 ${marginBottom}`}>
-      <label htmlFor={id}>{label}</label>
+    <div className={`flex flex-col gap-3 mb-6 ${className}`}>
       <input
-        id={id}
-        type={type}
-        name={name}
-        value={value}
-        autoComplete={autoComplete}
-        placeholder={placeholder}
-        onChange={onChange}
-        onBlur={onBlur}
+        {...rest}
         className={clsx(
           'px-[15px] py-[18px] h-[60px] border border-gray-300 rounded-lg bg-white',
-          isInvalid && 'border-red',
+          disabled && 'border-red',
         )}
       />
-      {isInvalid && <ErrorMessage text={errorMessage} />}
+      {disabled && <ErrorMessage text={errorMessage} />}
     </div>
   );
 };
+
+// Label 컴포넌트를 하위 컴포넌트로 등록
+InputWithError.Label = Label;
 export default InputWithError;
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -3,40 +3,33 @@ import InputWithError from '../../components/common/auth/InputWithError';
 import Button from '../login/Button';
 import api from '../../api/axios';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Signup: React.FC = () => {
   const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
   const [userName, setUserName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [isEmailValid, setIsEmailValid] = useState<boolean>(true);
-  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(true);
-  const [isUserNameValid, setIsUserNameValid] = useState<boolean>(true);
-  const [isConfirmPasswordValid, setIsConfirmPasswordValid] = useState<boolean>(true);
-
   const navigate = useNavigate();
 
   const handleSignupSuccess = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     // 폼 유효성 검사
     if (!isEmailValid || !isUserNameValid || !isPasswordValid || !isConfirmPasswordValid) {
       return;
     }
-
+    // API 로직
     const formData = {
       email,
       password,
       name: userName,
     };
-
     try {
       const response = await api.signUp(formData);
       localStorage.setItem('accessToken', response.data.accessToken);
       alert('회원가입 성공');
-      navigate('/login');
+      navigate('/links');
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) {
         console.log('서버 응답 : ', error.response.data);
@@ -45,35 +38,34 @@ const Signup: React.FC = () => {
     }
   };
 
-  const handleEmailInputValid = (e: React.FocusEvent<HTMLInputElement>) => {
-    const email = e.target.value;
+  const isUserNameValid = userName.length <= 10;
+  const isConfirmPasswordValid = password === confirmPassword;
+
+  const isEmailValid = useMemo(() => {
     const emailRegEx =
       /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
-    setIsEmailValid(emailRegEx.test(email));
-  };
+    if (email === '') {
+      return true;
+    } else {
+      return emailRegEx.test(email);
+    }
+  }, [email]);
 
-  const handleUserNameInputValid = (e: React.FocusEvent<HTMLInputElement>) => {
-    const userName = e.target.value;
-    setIsUserNameValid(userName.length <= 10);
-  };
-
-  const handlePasswordInputValid = (e: React.FocusEvent<HTMLInputElement>) => {
-    const password = e.target.value;
-    setIsPasswordValid(password.length >= 8);
-  };
-
-  const handleConfirmPasswordValid = (e: React.FocusEvent<HTMLInputElement>) => {
-    const confirmPassword = e.target.value;
-    setIsConfirmPasswordValid(password === confirmPassword);
-  };
+  const isPasswordValid = useMemo(() => {
+    if (password === '') {
+      return true;
+    } else {
+      return password.length >= 8;
+    }
+  }, [password]);
 
   return (
     <div className="flex flex-col items-center">
       <AuthPrompt prompt="이미 회원이신가요?" linkText="로그인 하기" linkTo="/login" />
 
       <form onSubmit={handleSignupSuccess}>
+        <InputWithError.Label label="이메일" id="user-email" />
         <InputWithError
-          label="이메일"
           id="user-email"
           type="email"
           name="email"
@@ -81,54 +73,47 @@ const Signup: React.FC = () => {
           placeholder="이메일을 입력하세요"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          marginBottom="mb-4"
-          onBlur={handleEmailInputValid}
-          isInvalid={!isEmailValid}
+          className="mb-4"
+          disabled={!isEmailValid}
           errorMessage="이메일 형식으로 작성해 주세요."
         />
 
+        <InputWithError.Label label="이름" id="user-name" />
         <InputWithError
-          label="이름"
-          id="user-name"
           type="text"
           name="user-name"
           autoComplete="username"
           placeholder="원하는 닉네임을 적어주세요"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
-          marginBottom="mb-4"
-          onBlur={handleUserNameInputValid}
-          isInvalid={!isUserNameValid}
+          className="mb-4"
+          disabled={!isUserNameValid}
           errorMessage="닉네임은 열 자 이하로 작성해주세요."
         />
 
+        <InputWithError.Label label="비밀번호" id="password" />
         <InputWithError
-          label="비밀번호"
-          id="password"
           type="password"
           name="password"
           autoComplete="new-password"
           placeholder="비밀번호를 입력하세요"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          marginBottom="mb-4"
-          onBlur={handlePasswordInputValid}
-          isInvalid={!isPasswordValid}
+          className="mb-4"
+          disabled={!isPasswordValid}
           errorMessage="비밀번호는 8자 이상 작성해 주세요."
         />
 
+        <InputWithError.Label label="비밀번호 확인" id="confirm-password" />
         <InputWithError
-          label="비밀번호 확인"
-          id="confirm-password"
           type="password"
           name="confirmPassword"
           autoComplete="new-password"
           placeholder="비밀번호를 한번 더 입력해 주세요"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
-          marginBottom="mb-[30px]"
-          onBlur={handleConfirmPasswordValid}
-          isInvalid={!isConfirmPasswordValid}
+          className="mb-[30px]"
+          disabled={!isConfirmPasswordValid}
           errorMessage="비밀번호가 일치하지 않습니다."
         />
         <Button text="회원가입" className="w-[400px] h-[53px] mb-8 " />
