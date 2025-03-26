a1e324b61f9fbf2bb8cc29e2e1237032c1eef10f
jjaneyxx <hmyeonge@gmail.com>
2025-03-27 01:46:56 +0900

    design: 로그아웃 버튼 추가

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,23 +1,18 @@
 import userProfile from '../assets/images/user-profile.svg';
-import { useState } from 'react';
 
 const UserMenu = () => {
-  const [isDropDownOpen, setIsDropDownOpen] = useState<boolean>(false);
-
-  const handleDropDownOpen = () => {
-    console.log('모달 열림');
-    setIsDropDownOpen(true);
-  };
   return (
     <div className="flex items-center gap-6">
       <button className="px-3 py-2.5 border border-[#6D6AFE] rounded-sm bg-[#E7EFFB] text-sm cursor-pointer">
         ⭐ 즐겨찾기
       </button>
-      <button className="flex items-center gap-1.5 cursor-pointer" onClick={handleDropDownOpen}>
+      <button className="px-3 py-2.5 border border-[#6D6AFE] rounded-sm bg-[#E7EFFB] text-sm cursor-pointer">
+        ☠️ 로그아웃
+      </button>
+      <button className="flex items-center gap-1.5 cursor-pointer relative">
         <img src={userProfile} alt="profile" />
         <span className="text-sm">이용섭</span>
       </button>
-      {isDropDownOpen && <div>열림</div>}
     </div>
   );
 };
