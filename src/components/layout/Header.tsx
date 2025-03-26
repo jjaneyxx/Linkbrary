53bd538d3ce65d263d4662ceb40c5f4eef98ebfa
jjaneyxx <hmyeonge@gmail.com>
2025-03-27 01:11:40 +0900

    design: 로그인 유무에 따른 header 조건부 렌더링

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,23 @@
+import { useEffect, useState } from 'react';
 import logo from '../../assets/logo.svg';
 import Button from '../common/Button';
 import { Link, useNavigate } from 'react-router-dom';
+import UserMenu from '../UserMenu';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    setIsLoggedIn(!!token);
+  }, []);
 
   const handleLoginClick = () => {
     // local storage 에 accessToken 이 있는지 확인 필요
     const accessToken = localStorage.getItem('accessToken');
 
     if (accessToken) {
-      console.log(accessToken);
       navigate('/login'); // 아래 구현 후 삭제
       // 즐겨찾기, 게정 정보 띄우기
     } else {
@@ -19,12 +26,16 @@ const Header = () => {
   };
 
   return (
-    <div className="bg-gray-100 px-[200px] py-5 ">
+    <div className="bg-gray-100 px-8 lg:px-[200px] py-5">
       <div className="flex justify-between items-center">
         <Link to="/">
-          <img src={logo} alt="logo" className="h-[24px]" />
+          <img src={logo} alt="logo" className="h-4 md:h-6" />
         </Link>
-        <Button text="로그인" className="w-[128px] h-[53px]" onClick={handleLoginClick} />
+        {isLoggedIn ? (
+          <UserMenu />
+        ) : (
+          <Button text="로그인" className="w-[128px] h-[53px]" onClick={handleLoginClick} />
+        )}
       </div>
     </div>
   );
