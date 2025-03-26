82ede563aba8980fbda782e084780cd52685e00b
해명 <hmyeonge@gmail.com>
2025-03-27 02:40:05 +0900

    Feat/auth : 로그아웃 및 요청 인터셉터 추가 (#9)
    * design: 로그인 유무에 따른 header 조건부 렌더링
    
    * design: 로그아웃 버튼 추가
    
    * feat: 로그아웃 기능 추가
    
    * feat: request interceptor 추가

---
+++ src/components/UserMenu.tsx
@@ -0,0 +1,26 @@
+import userProfile from '../assets/images/user-profile.svg';
+
+type UserMenuProps = {
+  onLogout: () => void;
+};
+
+const UserMenu = ({ onLogout }: UserMenuProps) => {
+  return (
+    <div className="flex items-center gap-6">
+      <button className="px-3 py-2.5 border border-[#6D6AFE] rounded-sm bg-[#E7EFFB] text-sm cursor-pointer">
+        ⭐ 즐겨찾기
+      </button>
+      <button
+        onClick={onLogout}
+        className="px-3 py-2.5 border border-[#6D6AFE] rounded-sm bg-[#E7EFFB] text-sm cursor-pointer"
+      >
+        ☠️ 로그아웃
+      </button>
+      <button className="flex items-center gap-1.5 cursor-pointer relative">
+        <img src={userProfile} alt="profile" />
+        <span className="text-sm">이용섭</span>
+      </button>
+    </div>
+  );
+};
+export default UserMenu;
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,30 +1,46 @@
+import { useEffect, useState } from 'react';
 import logo from '../../assets/logo.svg';
 import Button from '../common/Button';
 import { Link, useNavigate } from 'react-router-dom';
+import UserMenu from '../UserMenu';
 
 const Header = () => {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
-  const handleLoginClick = () => {
-    // local storage 에 accessToken 이 있는지 확인 필요
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    setIsLoggedIn(!!token);
+  }, []);
+
+  const handleLogin = () => {
     const accessToken = localStorage.getItem('accessToken');
 
-    if (accessToken) {
-      console.log(accessToken);
-      navigate('/login'); // 아래 구현 후 삭제
-      // 즐겨찾기, 게정 정보 띄우기
-    } else {
+    if (!accessToken) {
+      // token 이 없을 경우 바로 로그인 화면으로 이동
       navigate('/login');
+      return;
     }
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem('accessToken');
+    setIsLoggedIn(false);
+    alert('로그아웃 성공');
+    navigate('/login');
+  };
+
   return (
-    <div className="bg-gray-100 px-[200px] py-5 ">
+    <div className="bg-gray-100 px-8 lg:px-[200px] py-5">
       <div className="flex justify-between items-center">
         <Link to="/">
-          <img src={logo} alt="logo" className="h-[24px]" />
+          <img src={logo} alt="logo" className="h-4 md:h-6" />
         </Link>
-        <Button text="로그인" className="w-[128px] h-[53px]" onClick={handleLoginClick} />
+        {isLoggedIn ? (
+          <UserMenu onLogout={handleLogout} />
+        ) : (
+          <Button text="로그인" className="w-[128px] h-[53px]" onClick={handleLogin} />
+        )}
       </div>
     </div>
   );
