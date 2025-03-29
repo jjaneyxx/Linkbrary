e0172734438136b19bd51d6841d3b3719fd48097
jjaneyxx <hmyeonge@gmail.com>
2025-03-29 14:33:00 +0900

    fix: 모달 내부 클릭시 모달이 닫히는 문제 해결
    - 에러: 이벤트 버블링으로 모달 내부 클릭시 handleModalClose 실행
    - 해결: 모달 내부 클릭 시 e.stopPropagation( ) 오르 이벤트 전파를
      방지하는 함수 실행

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,13 +1,26 @@
 import { useModalStore } from '../../store/useModalStore';
 import Button from './Button';
 import closeModal from '../../assets/icons/close-modal.svg';
+import { MouseEvent } from 'react';
 
 const Modal = () => {
   const handleModalClose = useModalStore((state) => state.closeModal);
 
+  const preventEventBubbling = (e: MouseEvent<HTMLDivElement>) => {
+    // event bubbling 방지
+    e.stopPropagation();
+  };
+
   return (
-    <div className="fixed inset-0 z-40 bg-black/50" onClick={handleModalClose}>
-      <div className="absolute top-1/2 left-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center rounded-[15px] border border-[#CCD5E3] bg-white px-10 py-8">
+    <div
+      className="onClick={handleModalClose} fixed inset-0 z-40 bg-black/50"
+      onClick={handleModalClose}
+    >
+      {/* Modal */}
+      <div
+        onClick={preventEventBubbling}
+        className="absolute top-1/2 left-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center rounded-[15px] border border-[#CCD5E3] bg-white px-10 py-8"
+      >
         <div className="relative flex flex-col items-center">
           <span className="rounded-lg px-[15px] py-[18px] text-xl leading-1 font-bold text-[#373740]">
             폴더 추가
