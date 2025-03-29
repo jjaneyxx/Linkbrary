59bf5196dbdd11ac06508b163c57814396c869e1
jjaneyxx <hmyeonge@gmail.com>
2025-03-29 13:50:21 +0900

    design: 모달 z-index 값 수정, feat: 모달 오버레이 클릭 시 모달 닫힘 구현

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -6,8 +6,8 @@ const Modal = () => {
   const handleModalClose = useModalStore((state) => state.closeModal);
 
   return (
-    <div className="fixed inset-0 z-50 bg-black/50">
-      <div className="absolute top-1/2 left-1/2 z-40 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center rounded-[15px] border border-[#CCD5E3] bg-white px-10 py-8">
+    <div className="fixed inset-0 z-40 bg-black/50" onClick={handleModalClose}>
+      <div className="absolute top-1/2 left-1/2 z-50 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center rounded-[15px] border border-[#CCD5E3] bg-white px-10 py-8">
         <div className="relative flex flex-col items-center">
           <span className="rounded-lg px-[15px] py-[18px] text-xl leading-1 font-bold text-[#373740]">
             폴더 추가
