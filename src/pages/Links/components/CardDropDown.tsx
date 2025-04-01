8d1c400700e07e46fc00afb248120bd57baed90f
jjaneyxx <hmyeonge@gmail.com>
2025-04-01 16:54:35 +0900

    feat: 링쿠 수정 기능 구현

--- src/pages/Links/components/CardDropDown.tsx
+++ src/pages/Links/components/CardDropDown.tsx
@@ -1,19 +1,19 @@
 import clsx from 'clsx';
 import { useState } from 'react';
 import { useModalStore } from '../../../store/useModalStore';
-import { deleteLink } from '../../../api/link/api';
+import { deleteLink, putLink } from '../../../api/link/api';
 import { useParams } from 'react-router-dom';
 
 const CardDropDown = ({ isDropDownOpen }: { isDropDownOpen: boolean }) => {
   const [isSelected, setIsSelected] = useState<string>('');
   const openModal = useModalStore((state) => state.openModal);
   const closeModal = useModalStore((state) => state.closeModal);
+
   const { linkId } = useParams();
   const linkIdNumber = Number(linkId);
 
   const handleDeleteLink = async () => {
     try {
-      // test
       await deleteLink(linkIdNumber);
       alert('링크 삭제 성공');
       closeModal();
@@ -23,10 +23,38 @@ const CardDropDown = ({ isDropDownOpen }: { isDropDownOpen: boolean }) => {
     }
   };
 
+  const handlePutLink = async () => {
+    // brings recent input
+    const modalLinkInput = useModalStore.getState().input;
+    console.log(modalLinkInput);
+    if (!modalLinkInput) {
+      alert('유효한 링크를 입력하세요');
+      return;
+    }
+
+    // input 에 입력된 link
+    const linkData = {
+      url: modalLinkInput,
+    };
+
+    // test
+    try {
+      const response = await putLink(linkIdNumber, linkData);
+      console.log(response); // 수정된 링크 본문
+      closeModal();
+    } catch (error) {
+      alert('링크 수정 실패');
+      console.log('error', error);
+    }
+  };
+
   const handleDropDownSelected = (value: string) => {
     setIsSelected(value);
-    // 삭제하는 link 전달
-    openModal('링크 삭제', '삭제하기', handleDeleteLink, 'delete');
+    if (value === 'delete') {
+      openModal('링크 삭제', '삭제하기', handleDeleteLink, 'delete');
+    } else if (value === 'edit') {
+      openModal('링크 수정', '변경하기', handlePutLink);
+    }
   };
 
   if (isDropDownOpen === false) {
