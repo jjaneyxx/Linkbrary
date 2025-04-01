ea041eb1eb40fc0050ad593d41a5875977bb0e14
jjaneyxx <hmyeonge@gmail.com>
2025-04-02 05:57:30 +0900

    feat: add linkId to query string

--- src/pages/Links/components/links/LinkGallery.tsx
+++ src/pages/Links/components/links/LinkGallery.tsx
@@ -1,18 +1,32 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import Card from './Card';
+import { useFolderStore } from '@store/useFolderStore';
 import { useLinkStore } from '@store/useLinkStore';
+import { useNavigate } from 'react-router-dom';
+import Card from './Card';
 
 export const LinkGallery = () => {
   const linkList = useLinkStore((state) => state.linkList);
+  const setSelectedLinkId = useLinkStore((state) => state.setSelectedLinkId);
   const navigate = useNavigate();
-  const { folderId } = useParams();
+  const selectedFolderId = useFolderStore((state) => state.selectedFolderId);
 
   const handleLinkSelected = (linkId: number) => {
+    setSelectedLinkId(linkId);
+
     if (!linkId) {
       navigate(`/links`);
-    } else {
-      navigate(`/links/folder/${folderId}/link/${linkId}`);
+      return;
     }
+    // add query string
+    const params = new URLSearchParams();
+    params.set('link', linkId.toString());
+
+    // "전체" 탭에서 링크 선택
+    if (selectedFolderId === null) {
+      navigate(`?${params.toString()}`);
+      return;
+    }
+    // select link from selected folder
+    navigate(`?folder=${selectedFolderId}&${params.toString()}`);
   };
 
   return (
