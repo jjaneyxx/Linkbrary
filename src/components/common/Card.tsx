8aa21af9705e0d68da105ab43ff7decc6c2b36af
jjaneyxx <hmyeonge@gmail.com>
2025-04-01 15:07:07 +0900

    fix: 드롭다운 내부 클릭 시 드롭다운이 닫히는 문제 해결

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,7 +1,7 @@
 import { LinkResponse } from '../../api/link/api';
 import fallBackImage from '../../assets/images/card-fallback.svg';
 import kebap from '../../assets/icons/link-kebap.svg';
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
 import CardDropDown from '../../pages/Links/components/CardDropDown';
 
 type CardProps = {
@@ -10,10 +10,25 @@ type CardProps = {
 
 const Card = ({ link }: CardProps) => {
   const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const dropDownRef = useRef<HTMLDivElement>(null);
 
   const handleDropDown = (e: MouseEvent<HTMLButtonElement>) => {
     setIsDropDownOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (e: CustomEvent<MouseEvent>) => {
+      // dropdown menu 가 있고, 클릭된 요소가 dropdown menu 의 내부가 아닐 경우
+      if (dropDownRef.current && !dropDownRef.current.contains(e.target as Node)) {
+        setIsDropDownOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside as EventListener);
+    // cleanup
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside as EventListener);
+    };
+  }, []);
 
   return (
     <div className="flex h-[334px] w-full cursor-pointer flex-col rounded-[15px] border">
@@ -30,10 +45,12 @@ const Card = ({ link }: CardProps) => {
       <div className="flex-1 rounded-b-[15px] px-5 py-[15px]">
         <div className="mb-2.5 flex justify-between">
           <div>00 minutes ago</div>
-          <button className="relative cursor-pointer" onClick={handleDropDown}>
-            <img src={kebap} alt="kepab button" />
-            {isDropDownOpen && <CardDropDown isDropDownOpen={isDropDownOpen} />}
-          </button>
+          <div ref={dropDownRef}>
+            <button className="relative cursor-pointer" onClick={handleDropDown}>
+              <img src={kebap} alt="kepab button" />
+              {isDropDownOpen && <CardDropDown isDropDownOpen={isDropDownOpen} />}
+            </button>
+          </div>
         </div>
         <div className="line-clamp-2">{link.description}</div>
         <div>{link.createdAt}</div>
