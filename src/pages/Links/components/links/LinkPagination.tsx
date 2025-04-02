3be755e89ae4002cd2805e1e55aac9b5d75716c7
jjaneyxx <hmyeonge@gmail.com>
2025-04-02 17:59:42 +0900

    feat: maxPageNumber 에 따라 페이지네이션 번호 조건부 렌더링

--- src/pages/Links/components/links/LinkPagination.tsx
+++ src/pages/Links/components/links/LinkPagination.tsx
@@ -10,48 +10,27 @@ import page8 from '@assets/icons/pagination/page-8.svg';
 import page9 from '@assets/icons/pagination/page-9.svg';
 import rightArrow from '@assets/icons/right-arrow.svg';
 import { usePaginationStore } from '@store/usePaginationStore';
-import { useEffect } from 'react';
 
 export const LinkPagination = () => {
   const totalLinkCount = usePaginationStore((state) => state.totalCount);
-  useEffect(() => {
-    const maxPageNumber = Math.floor(totalLinkCount / 10) + 1;
-    console.log('maxPageNumber', maxPageNumber);
-    // maxPageNumber 에 따라 페이지 번호 렌더링
-  });
+
+  const maxPageNumber =
+    totalLinkCount % 9 === 0 ? Math.floor(totalLinkCount / 9) : Math.floor(totalLinkCount / 9) + 1;
+
+  const pageNumberList = Array.from({ length: maxPageNumber }, (_, i) => i + 1);
+  const pageNumberIcons = [page1, page2, page3, page4, page5, page6, page7, page8, page9];
 
   return (
     <div className="mt-10 flex justify-center gap-1">
       <button>
         <img src={leftArrow} className="mr-2.5 cursor-pointer" />
       </button>
-      <button>
-        <img src={page1} />
-      </button>
-      <button>
-        <img src={page2} />
-      </button>
-      <button>
-        <img src={page3} />
-      </button>
-      <button>
-        <img src={page4} />
-      </button>
-      <button>
-        <img src={page5} />
-      </button>
-      <button>
-        <img src={page6} />
-      </button>
-      <button>
-        <img src={page7} />
-      </button>
-      <button>
-        <img src={page8} />
-      </button>
-      <button>
-        <img src={page9} />
-      </button>
+      {pageNumberList.map((pageNumber) => (
+        <button key={pageNumber}>
+          <img src={pageNumberIcons[pageNumber - 1]} alt="pagination number" />
+        </button>
+      ))}
+
       <button>
         <img src={rightArrow} className="ml-2.5 cursor-pointer" />
       </button>
