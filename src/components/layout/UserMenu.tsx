df49d2d45653c6598136437bdcf938d3180a2869
jjaneyxx <hmyeonge@gmail.com>
2025-04-03 11:54:43 +0900

    design: 로그아웃 버튼 및 alert 창 메시지 변경

--- src/components/layout/UserMenu.tsx
+++ src/components/layout/UserMenu.tsx
@@ -1,4 +1,5 @@
 import userProfile from '@assets/icons/user-profile.svg';
+import Button from '@components/common/Button';
 
 type UserMenuProps = {
   onLogout: () => void;
@@ -14,15 +15,13 @@ type UserMenuProps = {
 const UserMenu = ({ onLogout, user }: UserMenuProps) => {
   return (
     <div className="flex items-center gap-6">
-      <button className="cursor-pointer rounded-sm border border-[#6D6AFE] bg-[#E7EFFB] px-3 py-2.5 text-sm">
+      <button
+        className="cursor-pointer rounded-sm border border-[#6D6AFE] bg-[#E7EFFB] px-3 py-2.5 text-sm"
+        onClick={() => alert('🙏 아직 준비 중인 기능이에요')}
+      >
         ⭐ 즐겨찾기
       </button>
-      <button
-        onClick={onLogout}
-        className="cursor-pointer rounded-sm border border-[#6D6AFE] bg-[#E7EFFB] px-3 py-2.5 text-sm"
-      >
-        ☠️ 로그아웃
-      </button>
+      <Button text="로그아웃" className="px-3 py-2.5 text-sm" onClick={onLogout} type="button" />
       <button className="relative flex cursor-pointer items-center gap-1.5">
         <img src={userProfile} alt="profile" />
         <span className="text-sm">{user?.name}</span>
