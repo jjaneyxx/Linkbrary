555a709f2fd37b3e5767a178eb064e8847d6aa86
jjaneyxx <hmyeonge@gmail.com>
2025-04-03 16:35:52 +0900

    design: footer 및 privacy, faq 페이지로 이동 구현

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,9 @@ import MainLayout from '@components/layout/MainLayout';
 import { AuthContextProvider } from '@contexts/AuthContext';
 import Login from '@pages/auth/LoginPage';
 import Signup from '@pages/auth/SignupPage';
+import { Faq } from '@pages/Landing/Faq';
 import Landing from '@pages/Landing/Landing';
+import { Privacy } from '@pages/Landing/Privacy';
 import LinkPage from '@pages/Links/LinkPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -15,6 +17,8 @@ const AppRouter = () => {
           <Route element={<MainLayout />}>
             <Route path="/" element={<Landing />}></Route>
             <Route path="/links" element={<LinkPage />}></Route>
+            <Route path="/privacy" element={<Privacy />}></Route>
+            <Route path="/faq" element={<Faq />}></Route>
           </Route>
 
           <Route element={<AuthLayout />}>
