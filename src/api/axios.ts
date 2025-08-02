28568b26deb8c6b4ca01e4d1ea04f99ec78ddea1
해명 <hmyeonge@gmail.com>
2025-08-02 20:55:49 +0900

    chore: apiClient 주석 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,23 @@
 import axios from 'axios';
 
+// apiClient : axios 인스턴스 (새로운 axios 객체) 를 만듬
+
 export const apiClient = axios.create({
+  // 모든 API 요청에서 공통 URL 자동 접두어
   baseURL: 'https://linkbrary-api.vercel.app/13-6',
+  // 기본 Content-Type 설정 : 요청의 body 가 JSON 형식으로 보내진다는 걸 서버에게 알림
   headers: { 'Content-Type': 'application/json' },
+});
+
+// request interceptor : token 있는 경우 헤더에 Bearer token 추가
+apiClient.interceptors.request.use((config) => {
+  // 로컬 스토리지에서 액세스 토큰을 가져와
+  const token = localStorage.getItem('accessToken');
+  if (token) {
+    // 각 요청의 Authorization 헤더에 Bearer 토큰을 추가
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 // 4xx error
@@ -10,12 +25,3 @@ export interface ErrorResponse {
   name: string;
   message: string;
 }
-
-// request interceptor : token 있는 경우 헤더에 Bearer token 추가
-apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem('accessToken');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
